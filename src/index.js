930c68f3c65c7f8a35c89b20c6bd4157a43548c6
MatveyRubets <rmatvey11@gmail.com>
2022-05-24 20:04:46 +0200

    added: logic tailwind

---
+++ src/index.js
@@ -0,0 +1,59 @@
+import axios from 'axios';
+import './sass/main.scss';
+
+const refs = {
+  searchForm: document.querySelector('.search-form'),
+  gallery: document.querySelector('.gallery'),
+};
+
+const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '27044661-5039fd8f86a9259a09df45cad';
+
+async function fetchCards(searchQuery) {
+  const res = await axios.get(
+    `${BASE_URL}?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true`,
+  );
+  return res.data;
+}
+
+function handleSearch(e) {
+  e.preventDefault();
+
+  const query = refs.searchForm.elements.searchQuery.value.trim();
+
+  fetchCards(query).then(renderMarkup);
+}
+
+function renderMarkup({ hits }) {
+  const markup = hits
+    .map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
+      return `
+      <a class="shadow rounded-sm" href="${largeImageURL}">
+        <img class=" object-fit" width="400" height="300" src="${webformatURL}" alt="${tags}">
+        <ul class="flex justify-around items-center">
+          <li class="flex flex-col items-center py-1">
+            <p>Likes</p>
+            <p>${likes}</p>
+          </li>
+          <li class="flex flex-col items-center py-1">
+            <p>Views</p>
+            <p>${views}</p>
+          </li>
+          <li class="flex flex-col items-center py-1">
+            <p>Comments</p>
+            <p>${comments}</p>
+          </li>
+          <li class="flex flex-col items-center py-1">
+            <p>Downloads</p>
+            <p>${downloads}</p>
+          </li>
+        </ul>
+    </a>
+
+      `;
+    })
+    .join('');
+  refs.gallery.insertAdjacentHTML('beforeend', markup);
+}
+
+refs.searchForm.addEventListener('submit', handleSearch);
